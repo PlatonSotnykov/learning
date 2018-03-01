026cafb776594d4821aa55cd08f6fa599473d60a
Platon Sotnykov <P.Sotnykov@adbglobal.com>
2018-03-01 23:22:58 +0100

    TimeLine implemented

--- epg_react/src/components/EpgChannel.jsx
+++ epg_react/src/components/EpgChannel.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Epg.css';
-import './EpgChannel.css';
 
 const EpgChannel = (props) => {
     const { number, name } = props;
 
     return (
         <div className = 'epg-left epg-container'>
-            <div className = 'epg-cell epg-channel-number'> {number} </div>
-            <div className = 'epg-cell epg-channel-name'> {name} </div>
+            <div className = 'epg-cell epg-left'> {number} </div>
+            <div className = 'epg-cell epg-right'> {name} </div>
         </div>
     );
 };
--- epg_react/src/components/EpgTimeLine.jsx
+++ epg_react/src/components/EpgTimeLine.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import './Epg.css';
 
+function formatTime(time) {
+    const d = new Date(time * 1000);
+    let hh = d.getHours().toString();
+    let mm = d.getMinutes().toString();
+
+    if (hh.length < 2) {
+        hh = '0' + hh;
+    }
+    if (mm.length < 2) {
+        mm = '0' + mm;
+    }
+    return hh + ':' + mm;
+}
+
 const EpgTimeLine = (props) => {
     const { startTime, timeFrameDuration, timeStep } = props;
+    const framesCount = timeFrameDuration / timeStep;
+    const timeLine = [];
+    let time = startTime;
+    const style = {
+        width: 100 / framesCount + '%'
+    };
 
-    return <div className = 'epg-cell epg-right'> EpgTimeLine </div>;
+    while (time < startTime + timeFrameDuration) {
+        timeLine.push(<div key = { time } style = { style } className = 'epg-cell'> { formatTime(time) } </div>);
+        time += timeStep;
+    }
+
+    return (
+        <div className = 'epg-container epg-right'>
+            { timeLine }
+        </div>
+    );
 };
 
 export default EpgTimeLine;
