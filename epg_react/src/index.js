4f32c979aed4661a575cfdfe27f1f042e3c569cb
Platon Sotnykov <P.Sotnykov@adbglobal.com>
2018-03-16 17:07:17 +0100

    Use Mousetrap for keys handling

--- epg_react/src/index.js
+++ epg_react/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Mousetrap from 'mousetrap';
+
+Mousetrap.bind('g', () => { console.log('[Guide]') });
 
 const AppRouter = () => (
     <BrowserRouter>
