5669d5476fe2acb2b6112e1d3d2de99186c217ff
Platon Sotnykov <P.Sotnykov@adbglobal.com>
2018-04-12 16:35:06 +0200

    Displaying of real week days implemented

--- epg_react/src/components/EpgDay.jsx
+++ epg_react/src/components/EpgDay.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import './Epg.css';
 
-const EpgDay = (props) => {
-    // const { startTime } = props;
+const DAY = 24 * 3600;
 
-    return <div className = 'epg-cell epg-left epg-day'>Today</div>;
+const WEEK_DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+
+function getDayOfWeek(time) {
+    const d = new Date();
+    const tzOffset = d.getTimezoneOffset() * 60;
+    const now = ~~(d.getTime() / 1000);
+    const todayDayIdx = ~~((now - tzOffset) / DAY);
+    const dayIdx = ~~((time - tzOffset) / DAY);
+
+    if (dayIdx === todayDayIdx) {
+        return 'Today';
+    } else if ((dayIdx - todayDayIdx) === 1) {
+        return 'Tomorrow';
+    } else {
+        return WEEK_DAYS[(new Date(time * 1000)).getDay()]
+    }
+}
+
+const EpgDay = (props) => {
+    const { startTime } = props;
+
+    return <div className = 'epg-cell epg-left epg-day'>{getDayOfWeek(startTime)}</div>;
 };
 
 export default EpgDay;
